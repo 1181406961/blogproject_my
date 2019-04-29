f5c94ad9029b7a25fbfab545cd8ef70d35d3f38e
root <root@host.localdomain>
2019-04-29 13:45:20 +0800

    添加requirement

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-z*76bv)-cigxsu)xhs3-l9rpxjs^z9vvi8o)2e!037p&ssm&i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,9 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
 HAYSTACK_CONNECTIONS = {
     'default':{
         'ENGINE':'blog.whoosh_cn_backend.WhooshEngine',
