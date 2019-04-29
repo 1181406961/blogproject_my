b3040f4e84bc06bbd249a67ef24dda5a22baee5b
chenjunwei <1181406961@qq.com>
2019-04-29 17:04:26 +0800

    测试时只允许本地访问

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-z*76bv)-cigxsu)xhs3-l9rpxjs^z9vvi8o)2e!037p&ssm&i'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True 
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
