a2cf8a888434f53c66b3dcb7fbbba5e20a08b8af
chenjunwei <1181406961@qq.com>
2019-04-29 14:58:47 +0800

    使用私有的settings

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-z*76bv)-cigxsu)xhs3-l9rpxjs^z9vvi8o)2e!037p&ssm&i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True 
 
 ALLOWED_HOSTS = ['*']
 
